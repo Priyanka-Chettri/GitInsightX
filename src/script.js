21f764b2b16bbc3cb9cdd53073d880fab815108f
Priyanka-Chettri <priyankachettri85@gmail.com>
2024-07-31 00:07:36 +0530

    Added till hamburger menu

---
+++ src/script.js
@@ -0,0 +1,23 @@
+
+const hamburger_button=document.getElementById('hamburger_button');
+const menu=document.getElementById('menu');
+const hero_section=document.getElementById('hero_section');
+//const lower_bg=document.getElementById('lower_bg');
+
+let toggle='on';
+
+hamburger_button.addEventListener('click',(e)=>{
+    console.log('Hamburger clicked ')
+    if(toggle=='on'){
+    menu.classList.remove('hidden')
+   // lower_bg.classList.remove('hidden')
+    toggle='off';
+    }
+    else if(toggle=='off'){
+        menu.classList.add('hidden')
+      //  lower_bg.classList.add('hidden')
+
+        toggle='on';
+        }
+})
+
