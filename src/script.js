f0cc1c99d3568df2e460057057b07cacd2693150
Priyanka-Chettri <priyankachettri85@gmail.com>
2024-08-07 23:58:08 +0530

    added chnage bg colour code in js, and ids in body and review us button

--- src/script.js
+++ src/script.js
@@ -2,22 +2,33 @@
 const hamburger_button=document.getElementById('hamburger_button');
 const menu=document.getElementById('menu');
 const hero_section=document.getElementById('hero_section');
-//const lower_bg=document.getElementById('lower_bg');
+const reviewUs=document.getElementById('reviewUs');
+const body=document.getElementById('body');
 
+let windowsColor='on'
 let toggle='on';
 
 hamburger_button.addEventListener('click',(e)=>{
     console.log('Hamburger clicked ')
     if(toggle=='on'){
     menu.classList.remove('hidden')
-  //  lower_bg.classList.remove('hidden')
     toggle='off';
     }
     else if(toggle=='off'){
         menu.classList.add('hidden')
-      //  lower_bg.classList.add('hidden')
-
         toggle='on';
         }
+})
+
+reviewUs.addEventListener('click',(e)=>{
+  console.log('Review Us clicked')
+  if(windowsColor=='on'){
+    body.classList.add('bg-gray-700')
+    windowsColor='off'
+  }
+  else if(windowsColor=='off'){
+    body.classList.add('bg-gray-700')  
+    windowsColor='on'
+  }
 })
 
